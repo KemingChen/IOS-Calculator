68195fa95ef8a60afa376dfcbf5e841abce29dcd
Keming <believe.75467@gmail.com>
2015-04-14 16:39:00 +0800

    link press number to function

--- calculator/ViewController.m
+++ calculator/ViewController.m
@@ -14,14 +14,22 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)clickNumber:(id)sender
+{
+    UIButton* button = (UIButton*)sender;
+    NSLog(@"%@", button.accessibilityHint);
 }
 
 @end
